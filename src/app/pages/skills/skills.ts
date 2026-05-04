c3014b7ba5c55fd68b7c0ddc41c6c1f4380c562f
Ahmed Elashrawy <91854800+ahmed-mahmoud-goda@users.noreply.github.com>
2026-05-04 20:47:23 +0000

    Finished skills page

---
+++ src/app/pages/skills/skills.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  imports: [],
+  templateUrl: './skills.html',
+  styleUrl: './skills.css',
+})
+export class Skills {
+  skills: any = [
+    {
+      title: 'Languages',
+      items: 'C#, JavaScript, TypeScript, Python, SQL'
+    },
+    {
+      title: 'Backend Development',
+      items: 'ASP.NET Core, Node.js, REST APIs, JWT Auth, Entity Framework Core, LINQ, Clean Architecture, Onion Architecture, Error Handling'
+    },
+    {
+      title: 'Frontend Development',
+      items: 'Angular, ReactJS, HTML, CSS, Tailwind CSS, Bootstrap'
+    },
+    {
+      title: 'Databases & Caching',
+      items: 'SQL Server, MongoDB, Redis'
+    },
+    {
+      title: 'Real-Time & Performance',
+      items: 'SignalR, Socket.io, Background Services, Caching Strategies, Query Optimization'
+    },
+    {
+      title: 'Tools & DevOps',
+      items: 'Git, GitHub, Visual Studio, VS Code, Postman, Docker, Docker Compose'
+    },
+    {
+      title: 'Cloud & AWS',
+      items: 'EC2, Elastic Beanstalk, ECS, ECR, S3'
+    }
+  ];
+}
